41ba58e2e2c30e6f8ef24ddfda27c55d9ceb5c43
AlbertoMartinezGar <albert.mtzz2003@gmail.com>
2022-03-10 13:23:21 -0600

    Get data with GraphQL

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,35 @@
-//import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import {PostCard, Categories, PostWidget} from "../components";
+import { PostCard, Categories, PostWidget } from '../components'
 
-const post = [
-  {title: 'React Testing', excerpt: 'Learn React Testing'},
-  {title: 'React with Tailwind', excerpt: 'React with Tailwind'}
-]
+import { getPosts } from '../services'
 
-const Home = () => {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className='lg:col-span-8 col-span-1'>
-          {post.map((post) => 
-              <PostCard post={post} key={post.title} />
-          )}
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'>
-              <PostWidget />
-              <Categories />
-            </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
-      
-
     </div>
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: { posts },
+  }
+}
