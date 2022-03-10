3f379e53d01f20f2b85cb9ba52b8cc75db118d45
AlbertoMartinezGar <albert.mtzz2003@gmail.com>
2022-03-10 13:03:58 -0600

    Header and more things

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+//import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import {PostCard, Categories, PostWidget} from "../components";
+
+const post = [
+  {title: 'React Testing', excerpt: 'Learn React Testing'},
+  {title: 'React with Tailwind', excerpt: 'React with Tailwind'}
+]
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className='lg:col-span-8 col-span-1'>
+          {post.map((post) => 
+              <PostCard post={post} key={post.title} />
+          )}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default Home
